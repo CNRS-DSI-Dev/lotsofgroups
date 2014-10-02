4a0744ab5fea6cceed8bb610a4be84836ee911db
Patrick Paysant <patrick.paysant@linagora.com>
2014-10-02 13:45:46 +0200

    Verify active state of lotsofgroups app when displaying admin user page.

--- js/app/lotsofgroups.js
+++ js/app/lotsofgroups.js
@@ -6,31 +6,48 @@
  * @license This file is licensed under the Affero General Public License version 3 or later. See the COPYING file.
  */
 
-var lotsfofgroups = angular.module('lotsofgroups', ['angucomplete-alt']);
-
-
-lotsfofgroups.controller('groupsController', ['$scope', function($scope) {
-    $scope.lotsofgroupsGroupsUrl = OC.generateUrl('/apps/lotsofgroups/api/1.0/groups/');
-
-    $scope.showGroup = function(item) {
-        if (item) {
-            GroupList.showGroup(item);
-            $('#groups_value').val('');
+$(document).ready(function() {
+    if (!OC.appswebroots.lotsofgroups2) {
+        var msg = '"LotsOfGroup" must be installed / enabled with this theme';
+        if (OC.theme.name) {
+            msg =  msg + ' (' + OC.theme.name + ')';
         }
+        OC.dialogs.alert(msg, 'WARNING');
     }
+});
 
-    $scope.showSearchGroup = function(item) {
-        if (item) {
-            GroupList.showGroup(item.originalObject.name);
-            $('#searchGroup').addClass('active');
+if (OC.appswebroots.lotsofgroups) {
+    var lotsfofgroups = angular.module('lotsofgroups', ['angucomplete-alt']);
+
+    lotsfofgroups.controller('groupsController', ['$scope', function($scope) {
+        $scope.lotsofgroupsGroupsUrl = OC.generateUrl('/apps/lotsofgroups/api/1.0/groups/');
+
+        $scope.showGroup = function(item) {
+            if (item) {
+                GroupList.showGroup(item);
+                $('#groups_value').val('');
+            }
         }
-    }
 
-    $scope.deleteGroup = function() {
-        var item = $('#groups_value').val();
-        if (item) {
-            GroupDeleteHandler.mark(item);
+        $scope.showSearchGroup = function(item) {
+            if (item) {
+                GroupList.showGroup(item.originalObject.name);
+                $('#searchGroup').addClass('active');
+            }
         }
-    }
 
-}]);
+        $scope.deleteGroup = function() {
+            var item = $('#groups_value').val();
+            if (item) {
+                GroupDeleteHandler.mark(item);
+            }
+        }
+
+    }]);
+}
+else {
+    // fake angular module to avoid any error msg (other than the first displayed on loading)
+
+    var lotsfofgroups = angular.module('lotsofgroups', []);
+    lotsfofgroups.controller('groupsController', ['$scope', function($scope) {}]);
+}
